1318c03b61e8d44133dc343252df9e66ed268b95
CarlGergi <carl.gergi@mail.utoronto.ca>
2025-09-08 16:53:59 -0400

    Added Multiples.java to count multiples of 3 or 5 below 1000

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,13 +2,26 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int count = 0;
-        for  (int i = 1; i < 100; i++) {
-           if (i % 5 ==0 || i % 3 == 0) {
-               count++;
-            }
-            System.out.println(count);
-        }
+        System.out.println(multiples(1000, 3, 5));
     }
 
-}
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+
+        if (a == b) {
+            for (int i = 1; i < n; i++) {
+                if (i % a == 0) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+    }
